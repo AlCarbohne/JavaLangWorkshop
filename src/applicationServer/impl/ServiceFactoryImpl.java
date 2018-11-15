c9513f91ddc4eda6467e2dfa485f74648e2dd44f
veri4912 <extern.vetle.riekert@esolutions.de>
2018-11-15 11:17:25 +0100

    implemented Service map and created stub for ServiceFactory

---
+++ src/applicationServer/impl/ServiceFactoryImpl.java
@@ -0,0 +1,15 @@
+package applicationServer.impl;
+
+import applicationServer.Service;
+import applicationServer.ServiceFactory;
+
+public class ServiceFactoryImpl implements ServiceFactory {
+    public ServiceFactoryImpl(String serviceName) {
+
+    }
+
+    @Override
+    public Service createService() {
+        return null;
+    }
+}
--- src/applicationServer/impl/ServiceMap.java
+++ src/applicationServer/impl/ServiceMap.java
@@ -1,4 +1,30 @@
 package applicationServer.impl;
 
-public class ServiceMap {
+import applicationServer.ServiceFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceMap implements applicationServer.ServiceMap {
+    private List<ServiceFactory> serviceList;
+    private static final List<String> serviceNames = new ArrayList<>();
+
+    public ServiceMap(List<ServiceFactory> serviceList) {
+        this.serviceList = new ArrayList<>();
+        populateServiceNames();
+        serviceNames.forEach(serviceName -> this.serviceList.add(new ServiceFactoryImpl(serviceName)));
+    }
+
+    private void populateServiceNames() {
+        serviceNames.add("ping");
+        serviceNames.add("uppercase");
+        serviceNames.add("timeofday");
+        serviceNames.add("scramble");
+        serviceNames.add("tacho");
+    }
+
+    @Override
+    public List<ServiceFactory> getServices() {
+        return this.serviceList;
+    }
 }
