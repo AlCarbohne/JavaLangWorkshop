986da324104c2751dc8ee00200afb10b4aca08d7
nise6080 <nicolas.seufert@esolutions.de>
2018-11-15 16:32:25 +0100

    changed Server so that its constructor no longer hangs, NOTE: to start the server, you now have to call its instance's run() method!

--- src/applicationServer/impl/Server.java
+++ src/applicationServer/impl/Server.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 
 public class Server {
@@ -20,7 +21,8 @@ public class Server {
     private static Map<String, ServiceFactory> serviceMap = ServiceMap.get();
 
     private ServerSocket listenerSocket;
-    private ExecutorService threadPool;
+    private final ExecutorService threadPool;
+    private final Semaphore connections;
 
     public static void main(String[] args) {
         new Server();
@@ -28,19 +30,10 @@ public class Server {
 
     public Server() {
         // bind to server port
+        connections = new Semaphore(MAX_CONNECTIONS);
         threadPool = Executors.newFixedThreadPool(MAX_CONNECTIONS);
         try {
             this.listenerSocket = new ServerSocket(LISTEN_PORT);
-            while (true) {
-                // TODO check the number of connections alive
-                try {
-                    Socket s = listenerSocket.accept();
-                    threadPool.submit(() -> runServerThread(s));
-                } catch (IOException e) {
-                    System.err.println("ERROR: encountered exception while trying to establish connection");
-                    e.printStackTrace();
-                }
-            }
         } catch (IOException e) {
             System.err.println("FATAL: encountered exception while trying to bind to listening port, exiting");
             e.printStackTrace();
@@ -48,7 +41,27 @@ public class Server {
         }
     }
 
-    public void runServerThread(Socket socket) {
+    public void run() {
+        while (true) {
+            // TODO check the number of connections alive
+            try {
+                // acquire license to accept a connection from the connections semaphore
+                // NOTE: it is the connection thread's responsibility to release this
+                // license, i.e. to call connections.release()!
+                connections.acquire();
+                Socket s = listenerSocket.accept();
+                threadPool.submit(() -> runServerThread(s));
+            } catch (IOException e) {
+                System.err.println("ERROR: encountered exception while trying to establish connection");
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                System.err.println();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void runServerThread(Socket socket) {
         // read from socket and
         // wait for application service request
         /*
@@ -71,6 +84,7 @@ public class Server {
                 Service service = sf.create(in, out);
                 service.start();
                 socket.close();
+                connections.release();
             }
         } catch (IOException e) {
             e.printStackTrace();
