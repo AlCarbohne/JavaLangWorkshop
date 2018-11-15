73d4a63e9d53f8e70341a12dcc5e8ec0f69296fe
nise6080 <nicolas.seufert@esolutions.de>
2018-11-15 16:20:02 +0100

    amended Server to use FixedThreadPool instead of its own class <ServerThread extends Thread>

--- src/applicationServer/impl/Server.java
+++ src/applicationServer/impl/Server.java
@@ -8,6 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Server {
@@ -27,54 +28,52 @@ public class Server {
 
     public Server() {
         // bind to server port
-//        threadPool = Executors.newFixedThreadPool(MAX_CONNECTIONS);
+        threadPool = Executors.newFixedThreadPool(MAX_CONNECTIONS);
         try {
             this.listenerSocket = new ServerSocket(LISTEN_PORT);
             while (true) {
                 // TODO check the number of connections alive
-                new ServerThread(this.listenerSocket.accept()).start();
+                try {
+                    Socket s = listenerSocket.accept();
+                    threadPool.submit(() -> runServerThread(s));
+                } catch (IOException e) {
+                    System.err.println("ERROR: encountered exception while trying to establish connection");
+                    e.printStackTrace();
+                }
             }
         } catch (IOException e) {
             System.err.println("FATAL: encountered exception while trying to bind to listening port, exiting");
-//            threadPool.shutdownNow();
+            e.printStackTrace();
+            threadPool.shutdownNow();
         }
     }
 
-    class ServerThread extends Thread {
-        private Socket socket;
-
-        private ServerThread(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            // read from socket and
-            // wait for application service request
-            /*
-                An application service request must be the first message from the client,
-                terminated by a newline. The message must contain the name of an available
-                service or the string "ls" to list all available services on the server.
-             */
-            try (
-                    BufferedReader in = new LoggingBufferedReader(new InputStreamReader(this.socket.getInputStream()));
-                    PrintWriter out = new LoggingPrintWriter(new OutputStreamWriter(this.socket.getOutputStream()))
-            ) {
-                String command = in.readLine();
-                ServiceFactory sf = serviceMap.get(command);
-                if (sf == null) {
-                    out.println("Service \"" + command + "\" does not exist");
-                    out.flush();
-                } else {
-                    out.println("OK");
-                    out.flush();
-                    Service service = sf.create(in, out);
-                    service.start();
-                    this.socket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void runServerThread(Socket socket) {
+        // read from socket and
+        // wait for application service request
+        /*
+            An application service request must be the first message from the client,
+            terminated by a newline. The message must contain the name of an available
+            service or the string "ls" to list all available services on the server.
+         */
+        try (
+                BufferedReader in = new LoggingBufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new LoggingPrintWriter(new OutputStreamWriter(socket.getOutputStream()))
+        ) {
+            String command = in.readLine();
+            ServiceFactory sf = serviceMap.get(command);
+            if (sf == null) {
+                out.println("Service \"" + command + "\" does not exist");
+                out.flush();
+            } else {
+                out.println("OK");
+                out.flush();
+                Service service = sf.create(in, out);
+                service.start();
+                socket.close();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
