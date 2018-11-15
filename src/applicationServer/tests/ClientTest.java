3ce5e92f842e1cc9d60013c6bb0badd3bb3b0d4b
veri4912 <extern.vetle.riekert@esolutions.de>
2018-11-15 17:04:50 +0100

    implemented testCases

--- src/applicationServer/tests/ClientTest.java
+++ src/applicationServer/tests/ClientTest.java
@@ -1,7 +1,6 @@
 package applicationServer.tests;
 
 import applicationServer.impl.Client;
-import applicationServer.impl.Server;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,30 +8,52 @@ class ClientTest {
 
     @Test
     void testPing() {
-        Thread thread = new Thread(() -> {
-            Server server = new Server();
-            server.run();
-        });
         Client client = new Client();
 
         Assertions.assertEquals("OK", client.sendRequests("ping"));
 
-        thread.interrupt();
         client.close();
     }
 
     @Test
     void testUppercase() {
-        Thread thread = new Thread(() -> {
-            Server server = new Server();
-            server.run();
-        });
         Client client = new Client();
 
         Assertions.assertEquals("OK", client.sendRequests("uppercase"));
         Assertions.assertEquals("ABCDEFGH1", client.sendRequests("aBcdeFGh1"));
 
-        thread.interrupt();
+        client.close();
+    }
+
+    @Test
+    void testScramble() {
+        Client client = new Client();
+
+        String command = "A AB abc abcd aBcdE";
+        String expected = "A AB abc acbd adcBE";
+        Assertions.assertEquals(expected, client.sendRequests(command));
+
+        client.close();
+    }
+
+    @Test
+    void testTacho() {
+        Client client = new Client();
+
+        Assertions.assertEquals("OK", client.sendRequests("uppercase"));
+        Assertions.assertEquals("ABCDEFGH1", client.sendRequests("aBcdeFGh1"));
+
+        client.close();
+    }
+
+    @Test
+    void testWrongService() {
+        Client client = new Client();
+
+        String command = "upperPing";
+        String expected = "Service \"\"" + command + "\"\" does not exist";
+        Assertions.assertEquals(expected, client.sendRequests(command));
+
         client.close();
     }
 
