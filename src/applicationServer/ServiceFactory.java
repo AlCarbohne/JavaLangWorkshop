47a1f414e078305eba348009463341597cfeea4c
veri4912 <extern.vetle.riekert@esolutions.de>
2018-11-15 13:55:46 +0100

    updated ServiceFactoryInterface

--- src/applicationServer/ServiceFactory.java
+++ src/applicationServer/ServiceFactory.java
@@ -1,7 +1,14 @@
 package applicationServer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public interface ServiceFactory {
-    Service create(Socket socket);
+    default Service create(Socket socket) throws IOException {
+        return create(socket.getInputStream(), socket.getOutputStream());
+    }
+
+    Service create(InputStream inputStream, OutputStream outputStream);
 }
