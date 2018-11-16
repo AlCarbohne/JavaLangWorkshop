59a3b868243a2881e313fd96da430c4c6bbc31c8
nise6080 <nicolas.seufert@esolutions.de>
2018-11-16 19:20:51 +0100

    yesterday's ScrambleService

--- src/applicationServer/services/ScrambleService.java
+++ src/applicationServer/services/ScrambleService.java
@@ -18,13 +18,29 @@ public class ScrambleService implements Service {
         try {
             String line;
             while (!(line = inputStream.readLine()).isEmpty()) {
-                // TODO actually do something here. anything.
-                outputStream.println(line);
+                // CAUTION: this only works on regular spaces
+                String newLine = "";
+                for (String word : line.split(" ")) {
+                    newLine += mangle(word) + " ";
+                }
+                // crop last space
+                outputStream.println(newLine.substring(0, newLine.length() - 1));
                 outputStream.flush();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return false;
+        return true;
+    }
+
+    private static String mangle(String word) {
+        char[] chars = word.toCharArray();
+        for (int i = 1; i < (chars.length / 2); i++) {
+            // swap inner characters pairwise
+            char c = chars[chars.length - (i+1)];
+            chars[chars.length - (i+1)] = chars[i];
+            chars[i] = c;
+        }
+        return String.valueOf(chars);
     }
 }
