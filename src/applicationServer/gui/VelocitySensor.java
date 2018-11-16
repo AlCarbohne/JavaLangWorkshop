598d66dc0a024ff5b956aa158b8580da13dcb6f5
nise6080 <nicolas.seufert@esolutions.de>
2018-11-16 09:15:20 +0100

    created GUI class VelocitySensor

---
+++ src/applicationServer/gui/VelocitySensor.java
@@ -0,0 +1,10 @@
+package applicationServer.gui;
+
+import javax.swing.*;
+
+public class VelocitySensor extends JFrame {
+    private static final String WINDOW_TITLE = "TachoUI";
+    public VelocitySensor() {
+        super(WINDOW_TITLE);
+    }
+}
